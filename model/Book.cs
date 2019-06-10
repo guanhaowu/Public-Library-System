09189fe139f4f673a9c4dd72520e3dedac4337b2
Guan Hao Wu <guanhao_wu@hotmail.com>
2019-06-11 00:44:28 +0200

    Public Library System template
    Public Library System template

---
+++ model/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLS
+{
+    class Book
+    {
+        public string Title;
+        public string Author;
+        public string Description;
+        public int Amount;
+        public string PublishYear;
+        public string ISBN;
+        public string[] Genre;
+        public string Edition;
+
+    }
+}
---
+++ model/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLS.model
+{
+    class Customer : Person
+    {
+        private string Username;
+
+        public Customer(
+            string surName, string lastName, string gender, string nationality, string street,
+            string postcode, string city, string email, string telephoneNum, string username 
+            ) : base(surName, lastName, gender, nationality, street,
+            postcode, city, email, telephoneNum)
+        {
+
+        }
+    }
+}
